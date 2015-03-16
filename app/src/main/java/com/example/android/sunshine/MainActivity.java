279353972c0d0bdbec6096c75420690770eac9b8
Fernanda <fggeraissate@gmail.com>
2015-03-16 14:13:37 -0300

    4.01 Lifecycle Events quiz adding log statements

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,8 +15,23 @@ public class MainActivity extends ActionBarActivity {
 
     private final  String LOG_TAG = MainActivity.class.getSimpleName();
 
+    // -- Lifecycle --
+
+    // onCreate (created) - you build and wire up your UI. Once that's done, your activity is created.
+    // onStart (visible) - when the activity becomes visible. <---------------------------|
+    // onResume (visible) - when it gets focus and becomes the active foreground app.     |
+    //                                                                                    |
+    // onPause (partially visible) - when the activity lost focus                         |
+    // onStop (hidden)- when the app is no longer visible. -----------------> onRestart --|
+    // onDestroy (destroyed) - indicates the end of the app cycle
+
+    // Maintaining State (*)
+    // Active -> *onSaveInstanceState -> onPause -> Terminated ----|
+    //                     *onRestoreInstanceStage <- onCreate <---|
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        Log.v(LOG_TAG, "in onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // We inflate the layout and associate it with the Activity
         if (savedInstanceState == null) {
@@ -25,6 +40,43 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
+
+//    @Override
+//    protected void onStart() {
+//        Log.v(LOG_TAG, "in onStart");
+//        super.onStart();
+//        // The activity is about to become visible.
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        Log.v(LOG_TAG, "in onResume");
+//        super.onResume();
+//        // The activity has become visible (it is now "resumed").
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        Log.v(LOG_TAG, "in onPause");
+//        super.onPause();
+//        // Another activity is taking focus (this activity is about to be "paused").
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        Log.v(LOG_TAG, "in onStop");
+//        super.onStop();
+//        // The activity is no longer visible (it is now "stopped")
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        Log.v(LOG_TAG, "in onDestroy");
+//        super.onDestroy();
+//        // The activity is about to be destroyed.
+//    }
+
+    // -- End Lifecycle --
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
